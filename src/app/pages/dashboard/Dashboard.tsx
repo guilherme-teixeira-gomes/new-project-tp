1162329cbdb6dbed21088dad2ca23dd6d0bde150
Guilherme Teixeira Gomes <guilherme.tgomes@hotmail.com>
2022-10-20 14:45:05 -0300

    feat: add login page form

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,13 @@
-
+import {Link} from 'react-router-dom';
 
 export const Dashboard = () => {
 
     return (
+       <div>
         <p>Dashboard</p>
+        <Link to="/entrar"> Login </Link>
+        </div>
+
     )
 
 }
